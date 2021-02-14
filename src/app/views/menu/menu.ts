aa1bd7ae2e77e3f44eda13a53413cb87a18ae84a
ilbis <ilbis.candido@gmail.com>
2021-02-14 10:20:13 -0300

    Fix menu

--- src/app/views/menu/menu.ts
+++ src/app/views/menu/menu.ts
@@ -1,15 +1,7 @@
-import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthenticationService } from 'src/app/services/authentication.service';
-import { ProdutoService } from 'src/app/services/produto.service';
-import { Produto } from 'src/app/model/produto';
-import { MatTableDataSource } from '@angular/material/table';
-import { MatSort } from '@angular/material/sort';
 import { MatDialog } from '@angular/material/dialog';
-import { DialogData } from '../utils/dialog.component';
-import { PedidoService } from 'src/app/services/pedido.service';
-import { ItemPedido } from 'src/app/model/item-pedido';
-import { Pedido } from 'src/app/model/pedido';
 
 @Component({
   selector: 'app-menu',
